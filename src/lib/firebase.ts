e7f687e0a16ccefdc7dff3e7255d9c19b5f331ce
Sahil-Khilari <167540780+Sahil-Khilari@users.noreply.github.com>
2025-06-27 22:04:10 +0530

    Updated firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,7 +12,7 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+export const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
